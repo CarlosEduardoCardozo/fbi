6f8a0b232543a0905890478593801dc90d2625a0
cardozobento <99202104+GCFilmes@users.noreply.github.com>
2022-09-12 15:27:48 -0300

    Add files via upload

---
+++ procuradospelafbi/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from "vue-router";
+import HeaderComp from "./components/headerComp.vue";
+</script>
+
+<template>
+  <header></header>
+  <HeaderComp />
+  <RouterView />
+</template>
+
+<style scoped></style>
